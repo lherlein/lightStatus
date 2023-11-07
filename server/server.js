028687376ca17ea22759095844d21bdeb4e555a3
lherlein <luca@herlein.com>
2023-11-07 09:27:53 -0800

    installed nx, updated lint, package.json

--- server/server.js
+++ server/server.js
@@ -1,26 +1,22 @@
-import * as http from 'http';
-import * as fs from 'fs';
+const http = require('http');
+const fs = require('fs').promises;
 
 // create server
 const server = http.createServer((req, res) => {
-  server.on('request', (req, res) => {
-    if (req.url === '/' || req.url === '/index.html') {
-        // Read the HTML file and send it as a response
-        fs.readFile('../../index.html', (err, data) => {
-          if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-          } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-          }
-        });
-      }
-  });
+  if (req.url === '/' || req.url === '/index.html') {
+    fs.readFile('../index.html')
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('Hello World!');
+    res.end();
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.write('404 Not Found');
+    res.end();
+  }
 });
 
 
-const port = 8000;
+const port = 13500;
 const hostname = 'localhost'
 
 server.listen(port, hostname, () => {

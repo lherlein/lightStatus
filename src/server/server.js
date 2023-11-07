3e4366d7934364e87c57b99a6f9edd010845720e
lherlein <luca@herlein.com>
2023-11-07 11:01:32 -0800

    server is working

--- src/server/server.js
+++ src/server/server.js
@@ -1,25 +1,42 @@
 const http = require('http');
 const fs = require('fs').promises;
 
-const data = fs.readFile('./index.html');
-
-// create server
-const server = http.createServer((req, res) => {
-  if (req.url === '/' || req.url === '/index.html') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    res.end();
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.write('404 Not Found');
-    res.end();
-  }
-});
-
-
 const port = 13500;
 const hostname = 'localhost'
 
-server.listen(port, hostname, () => {
+function runServer(data) {
+// create server
+  const server = http.createServer((req, res) => {
+    if (req.url === '/' || req.url === '/index.html') {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      //res.write(data);
+      res.end(data);
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      res.write('404 Not Found');
+      res.end();
+    }
+  });
+
+  server.listen(port, hostname, () => {
     console.log(`Server is running at http://${hostname}:${port}/`);
-});
+  });
+}
+
+async function getHtml() {
+  const data = await fs.readFile(`${__dirname}/index.html`);
+  return data;
+}
+
+async function main() {
+  const data = await getHtml();
+  runServer(data);
+}
+
+main();
+
+
+
+
+
+
